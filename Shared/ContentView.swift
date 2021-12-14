7c4a18d59866029b2209164f21ce7de98c73507a
fuwamaki <variety.fuwa.3@gmail.com>
2021-12-14 10:28:39 +0900

    add Subject

---
+++ Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by 牧宥作 on 2021/12/14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/SampleSwiftUI2App.swift
@@ -0,0 +1,17 @@
+//
+//  SampleSwiftUI2App.swift
+//  Shared
+//
+//  Created by 牧宥作 on 2021/12/14.
+//
+
+import SwiftUI
+
+@main
+struct SampleSwiftUI2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
