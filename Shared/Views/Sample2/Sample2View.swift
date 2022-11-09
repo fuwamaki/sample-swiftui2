37ca62b70232743dc5ab40e33d2081aa90c7d9e7
fuwamaki <variety.fuwa.3@gmail.com>
2022-11-09 13:29:49 +0900

    add CustomTextView

--- Shared/Views/Sample2/Sample2View.swift
+++ Shared/Views/Sample2/Sample2View.swift
@@ -6,9 +6,11 @@
 //
 
 import SwiftUI
+import UIKit
 
 struct Sample2View: View {
     @State var isRounded: Bool = false
+    @State var text: String = "サンプルテキスト\nhttps://www.google.com\nです"
 
     var body: some View {
         VStack {
@@ -17,6 +19,8 @@ struct Sample2View: View {
                 .background(.yellow)
                 .clipShape(Capsule())
                 .modifier(CustomShape(isRounded: isRounded))
+            CustomTextView(text: $text)
+            Spacer()
         }
     }
 }
@@ -38,5 +42,44 @@ struct CustomShape: ViewModifier {
 struct Sample2View_Previews: PreviewProvider {
     static var previews: some View {
         Sample2View()
+    }
+}
+
+
+struct CustomTextView: UIViewRepresentable {
+    let textView = UITextView()
+    @Binding var text: String
+
+    func makeUIView(context: Context) -> UITextView {
+        textView.delegate = context.coordinator
+        textView.isEditable = false
+        textView.isSelectable = true
+        textView.font = .systemFont(ofSize: 14)
+        textView.textColor = .darkGray
+        textView.linkTextAttributes = [
+            NSAttributedString.Key.foregroundColor: UIColor.link
+        ]
+        textView.dataDetectorTypes = .link
+        return textView
+    }
+
+    func updateUIView(_ uiView: UITextView, context: Context) {
+        uiView.text = text
+    }
+
+    func makeCoordinator() -> Coordinator {
+        Coordinator(text: $text)
+    }
+
+    class Coordinator: NSObject, UITextViewDelegate {
+        @Binding var text: String
+
+        init(text: Binding<String>) {
+            self._text = text
+        }
+
+        func textViewDidChange(_ textView: UITextView) {
+            _text.wrappedValue = textView.text
+        }
     }
 }
