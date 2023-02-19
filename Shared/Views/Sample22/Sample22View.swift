a563a9e6427c260fcf34c9a947b85524bc48be66
fuwamaki <variety.fuwa.3@gmail.com>
2023-02-19 21:28:00 +0900

    fix Sample22View

--- Shared/Views/Sample22/Sample22View.swift
+++ Shared/Views/Sample22/Sample22View.swift
@@ -14,36 +14,14 @@ struct Sample22View: View {
     var body: some View {
         ZStack {
             VStack {
-                Text("aaa")
+                Text("space")
             }
             .frame(maxWidth: .infinity, maxHeight: .infinity)
             .contentShape(Rectangle())
             .onTapGesture {
                 isFocus = false
             }
-            VStack(spacing: 0) {
-                Spacer()
-                Divider()
-                HStack(alignment: .bottom) {
-                    TextField("TextField",text: self.$text, axis: .vertical)
-                        .padding(8)
-                        .lineLimit(1...10)
-                        .frame(minHeight: 40)
-                        .background(Color.gray.opacity(0.2))
-                        .cornerRadius(8.0)
-                        .focused($isFocus)
-                    Button {
-                    } label: {
-                        Text("Send")
-                            .frame(width: 72, height: 40)
-                            .background(Color.cyan)
-                            .foregroundColor(.white)
-                            .cornerRadius(8.0)
-                    }
-                }
-                .padding(8)
-                .background(Color.white)
-            }
+            MessageInputView(text: $text, isFocus: _isFocus)
         }
         .navigationTitle("Test")
     }
@@ -52,5 +30,36 @@ struct Sample22View: View {
 struct Sample22View_Previews: PreviewProvider {
     static var previews: some View {
         Sample22View()
+    }
+}
+
+struct MessageInputView: View {
+    @Binding var text: String
+    @FocusState var isFocus: Bool
+
+    var body: some View {
+        VStack(spacing: 0) {
+            Spacer()
+            Divider()
+            HStack(alignment: .bottom) {
+                TextField("TextField",text: self.$text, axis: .vertical)
+                    .padding(8)
+                    .lineLimit(1...10)
+                    .frame(minHeight: 40)
+                    .background(Color.gray.opacity(0.2))
+                    .cornerRadius(8.0)
+                    .focused($isFocus)
+                Button {
+                } label: {
+                    Text("Send")
+                        .frame(width: 72, height: 40)
+                        .background(Color.cyan)
+                        .foregroundColor(.white)
+                        .cornerRadius(8.0)
+                }
+            }
+            .padding(8)
+            .background(Color.white)
+        }
     }
 }
